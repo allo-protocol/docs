b30124247e061a10a58ef4b3f010e7656499a0b1
Zakk <zakk@hawthorne.io>
2023-05-24 07:07:48 -0500

    initial setup for nextra docs site

---
+++ theme.config.jsx
@@ -0,0 +1,7 @@
+export default {
+  logo: <span>Allo Protocol</span>,
+  project: {
+    link: 'https://github.com/allo-protocol'
+  },
+  docsRepositoryBase: 'https://github.com/allo-protocol/docs',
+}
