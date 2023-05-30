680fdf0bc322357da9c72b8f37ac6ead2b70348e
Zakk <zakk@hawthorne.io>
2023-05-30 10:59:38 -0500

    touch up merge issue

--- theme.config.jsx
+++ theme.config.jsx
@@ -3,13 +3,5 @@ export default {
   project: {
     link: 'https://github.com/allo-protocol'
   },
-<<<<<<< HEAD
-<<<<<<< HEAD
   docsRepositoryBase: 'https://github.com/allo-protocol/docs/blob/main',
-=======
-  docsRepositoryBase: 'https://github.com/allo-protocol/docs',
->>>>>>> 157ff73 (initial setup for nextra docs site)
-=======
-  docsRepositoryBase: 'https://github.com/allo-protocol/docs/blob/main',
->>>>>>> 654ac7c (theme and page cleanup after migrating from gitbook)
 }
