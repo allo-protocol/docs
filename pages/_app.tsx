c3863176a35c21666648db148aa0af728b0c8122
Zakk <zakk@hawthorne.io>
2023-07-26 13:58:46 -0500

    set up tailwind and add logo to homepage

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import '../global.css'
+
+export default function Nextra({ Component, pageProps }) {
+  const getLayout = Component.getLayout || ((page) => page)
+  return getLayout(
+    <Component {...pageProps} />
+  )
+}
