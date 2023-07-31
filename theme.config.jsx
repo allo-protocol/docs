f7d7911782ef54482b77b27ddeb86160fce96667
Zakk <zakk@hawthorne.io>
2023-07-31 10:29:46 -0500

    add current status section to homepage

--- theme.config.jsx
+++ theme.config.jsx
@@ -8,7 +8,7 @@ export default {
     key: '',
     text: (
       <a href="https://github.com/allo-protocol/allo-v2/" target="_blank" rel="noreferrer">
-        Allo v2 is under active development. Learn more →
+        🚧 Allo v2 is under active development. Learn more →
       </a>
     )
   }
