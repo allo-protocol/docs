ae477a4b852b484b9d878fa86f5b40cd1e34977e
Henry Wilson <wilsonhenry@rogers.com>
2024-06-24 16:50:00 -0400

    fix link previews

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,20 +1,33 @@
+import { useRouter } from 'next/router'
+import { useConfig } from 'nextra-theme-docs'
+
 export default {
   logo: <span>Allo Protocol</span>,
   project: {
     link: 'https://github.com/allo-protocol'
   },
   docsRepositoryBase: 'https://github.com/allo-protocol/docs/blob/main',
-  banner: {
-    key: '',
-    text: (
-      <a href="/#current-status" target="_blank" rel="noreferrer">
-        🚧 Allo v2 is under active development. Learn more →
-      </a>
-    )
-  },
   useNextSeoProps() {
     return {
-      titleTemplate: '%s – Allo'
+      titleTemplate: '%s – Allo Docs'
     }
+  },
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter()
+    const { frontMatter } = useConfig()
+    const url =
+      'https://docs.allo.gitcoin.co' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+ 
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={frontMatter.title || 'Allo'} />
+        <meta
+          property="og:description"
+          content={frontMatter.description || 'The Allo protocol docs'}
+        />
+      </>
+    )
   }
 }
